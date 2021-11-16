c44406d118b40ab89698950920625a7ceae564cb
keeshcodes <94489564+keeshcodes@users.noreply.github.com>
2021-11-16 15:18:49 -0600

    Update StockChartBuilder.py

--- StockChartBuilder.py
+++ StockChartBuilder.py
@@ -17,7 +17,7 @@ end = dt.datetime.now()
 #i.e. Avast PLC (London) = AVST.L
 #Reference Guide: https://help.yahoo.com/kb/SLN2310.html?guccounter=1&guce_referrer=aHR0cHM6Ly93d3cuZ29vZ2xlLmNvbS8&guce_referrer_sig=AQAAAL8WnOj-I2UQJnpHK83SUYny-XoIJ9DdO0-UTgzDxhz_aHrJaniUo2lFzgw3LtLAbroWicGEs1n_P1F3vgC3y6DiM3R-usiaMa70Ou__yk5WmBajzP8UyQPRnldsBN8AsDdbqRW7tQwvQ3ZqcSdetij0vyIaallcnDrmKFlw_iWN
 
-df=web.get_data_yahoo("BRSAN.IS",start,end)
+df=web.get_data_yahoo("AVST.L",start,end)
 #Plot Stock based on Adj. Close
 #rcParams['figure.figsize'] = 10, 6
 
